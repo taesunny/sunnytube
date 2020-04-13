676054ca8384c84ac38bb62d315ba7aae999ece6
Taesun Lee <superbsun@gmail.com>
2020-04-13 20:50:26 +0900

    use router
    - add init.js, change index.js to app.js
    - use router

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+const middleware = (req, res, next) => {
+  res.send("not happening");
+};
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
