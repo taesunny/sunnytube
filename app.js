66cc78e70ca1821d30c6e7aa60ea781deb2182cc
Taesun Lee <superbsun@gmail.com>
2020-04-14 20:33:52 +0900

    add middlewares for use locals variables global

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
 app.set("view engine", "pug");
 
+app.use(helmet()); // place it to the top to be safe
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(helmet());
+
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
